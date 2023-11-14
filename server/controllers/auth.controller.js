d04145c7b0d12b1ccbc14a934130f5afb1046a0c
Amal Das <amaldassk@gmail.com>
2023-11-14 21:13:15 +0530

    signin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,68 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import userModel from '../models/user.model.js';
 
-const signinUser = async(req, res) => {
+const signinUser = async(req, res, next) => {
+    try{
+        const { email,password } = req.body;
+        const existingUser = await userModel.findOne({
+            email: {$eq: email},
+        });
+        
+        if(!existingUser){
+            return res.status(400).json({
+                message:"Invalid credentials",
+            });
+        }
 
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            existingUser.password
+        );
+
+        if(!isPasswordCorrect){
+            return res.status(400).json({
+                message:"Invalid credentials",
+            });
+        }
+
+        const payload = {
+            id: existingUser.id,
+            email: existingUser.email,
+        }
+
+        const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '6h',
+        });
+
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+            expiresIn: '7h',
+        });
+
+        // const newRefreshToken = new Token({
+        //     user: existingUser._id,
+        //     accessToken,
+        //     refreshToken,
+        // });
+        // await newRefreshToken.save();
+        // console.log(newRefreshToken)
+        res.status(200).json({
+            accessToken,
+            refreshToken,
+            accessTokenUpdatedAt: new Date().toLocaleString(),
+            user:{
+                _id: existingUser._id,
+                name: existingUser.name,
+                email: existingUser.email,
+                role: existingUser.role,
+                avatar: existingUser.avatar,
+            },
+        });
+    } catch(err){
+        res.status(500).json({
+            message:"Something went wrong",
+        });
+    }
 }
 
 const registerUser = async(req, res, next) => {
