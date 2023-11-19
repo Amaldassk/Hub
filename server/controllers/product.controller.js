b5d53fb31d6e495c70a390f572a0ac62d75f2de9
Amal Das <amaldassk@gmail.com>
2023-11-19 16:32:46 +0530

    update

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,6 +1,7 @@
 import productModel from "../models/product.model.js"
 
 const createProduct = async(req, res) => {
+    const {id} = req.params
     try{
         const newProduct = await productModel.create(req.body);
         res.json(newProduct);
@@ -10,11 +11,21 @@ const createProduct = async(req, res) => {
 }
 
 const getProduct = async() => {
-
+    try{
+        const getAProduct = await productModel.findById(id);
+        res.json(getAProduct);
+    } catch(err){
+        throw new Error(err);
+    }
 }
 
 const getAllProducts = async() => {
-
+    try{
+        const getProducts = await productModel.find();
+        res.json(getAllProducts);
+    } catch(err){
+        throw new Error(err);
+    }
 }
 
 const deleteProduct = async() => {
