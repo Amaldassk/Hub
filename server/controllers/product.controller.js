703b7b3dc472729a460ad9a8910adcb99225df09
Amal Das <amaldassk@gmail.com>
2023-11-19 16:37:20 +0530

    update

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -10,7 +10,7 @@ const createProduct = async(req, res) => {
     }
 }
 
-const getProduct = async() => {
+const getProduct = async(req, res) => {
     try{
         const getAProduct = await productModel.findById(id);
         res.json(getAProduct);
@@ -19,21 +19,33 @@ const getProduct = async() => {
     }
 }
 
-const getAllProducts = async() => {
+const getAllProducts = async(req, res) => {
     try{
         const getProducts = await productModel.find();
-        res.json(getAllProducts);
+        res.json(getProducts);
     } catch(err){
         throw new Error(err);
     }
 }
 
-const deleteProduct = async() => {
-
+const deleteProduct = async(req, res) => {
+    const {id} = req.body;
+    try{
+        const deleteProduct = await productModel.findByIdAndDelete(id);
+        res.json(deleteProduct);
+    } catch(err){
+        throw new Error(err);
+    }
 }
 
 const updateProduct = async() => {
-
+    const {id} = req.body;
+    try{
+        const updateProduct = await productModel.findByIdAndUpdate(id. req.body,{new:true});
+        res.json(updateProduct);
+    } catch(err){
+        throw new Error(err);
+    }
 }
 
 export {createProduct, getProduct, getAllProducts, deleteProduct, updateProduct}
